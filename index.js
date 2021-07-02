5ad25631e604cf5c73da5ac684c4c0fec96d7f64
Piyush Pandey <piyushpvns@gmail.com>
2021-07-02 23:03:19 +0530

    Create Node Module

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 console.log("Hello World");
 
-function add(a, b){
-    return a + b;
+function add(a, b) {
+  return a + b;
 }
 
 console.log(add(2, 8));
 
 console.log(process.argv);
+
+var args = process.argv.slice(2);
+
+console.log("Adding the numbers: ", add(parseInt(args[0]), parseInt(args[1])));
