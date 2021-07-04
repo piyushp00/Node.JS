6b38cf1c2373b4d915bff76b0ec8f5ec2b26e836
Piyush Pandey <piyushpvns@gmail.com>
2021-07-04 09:57:16 +0530

    Update Index.JS

--- BasicHttpServer/index.js
+++ BasicHttpServer/index.js
@@ -1,1 +1,22 @@
-const http = require 
+const http = require('http');
+const port = 8000;
+
+function requestHandler(req, res){
+    console.log(req.url);
+
+    res.end('Gotcha');
+}
+
+const server = http.createServer(requestHandler);
+
+//port-unique id for service on ur system=i
+server.listen(port, function(err){
+    if(err){
+        console.log(err);
+        return;
+    }
+
+    console.log("Server is up and running on port", port);
+}); 
+
+//localhost is same as 127.0.0.1
