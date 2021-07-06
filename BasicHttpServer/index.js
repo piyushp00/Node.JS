0a1d4ab87b6610f823cb078eec0bef07377f155c
Piyush Pandey <piyushpvns@gmail.com>
2021-07-07 00:15:28 +0530

    Update index.js

--- BasicHttpServer/index.js
+++ BasicHttpServer/index.js
@@ -7,6 +7,37 @@ function requestHandler(req, res){
     console.log(req.url);
     res.writeHead(200, {'Content-Type': 'text/html'}); //For writing in Response headers
 
+    let filePath;
+
+    switch(req.url){
+        case '/':
+            filePath = './index.html'
+            break;
+        case '/profile':
+            filePath = './profile.html'
+            break;
+
+        default:
+            filePath = './404.html'
+    }
+
+    fs.readFile(filePath, function(err, data){
+        if(err){
+            console.log('error', err);
+            return res.end('<h1 style="color: orange;">Error!! Do Not Panic </h1>')
+        }
+
+        return res.end(data);
+    });
+}
+
+
+//for index.js
+/*
+function requestHandler(req, res){
+    console.log(req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'}); //For writing in Response headers
+
     fs.readFile('./index.html', function(err, data){
         if(err){
             console.log('error', err);
@@ -18,7 +49,7 @@ function requestHandler(req, res){
 
     //res.end('<h1 style="color: red; border: 1px solid black;">Gotcha!</h1>');
 }
-
+*/
 const server = http.createServer(requestHandler);
 
 //port-unique id for service on ur system=i
