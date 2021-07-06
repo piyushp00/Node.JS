b26267ea2451868906d7997a15fc9efc2c04748e
Piyush Pandey <piyushpvns@gmail.com>
2021-07-07 00:13:37 +0530

    Create Contact List

--- Contact_List/index.js
+++ Contact_List/index.js
@@ -1,11 +1,32 @@
+//code order -> 1.ibraries - 2.different files and properties- 3.middleware
 const express = require('express');
+//library for storing views(comes in built)
+const path = require('path');
 const port = 8000;
 
 const app = express();
 
+//setting up template engine
+app.set('view engine', 'ejs');
+
+//store views/templates
+app.set('views', path.join(__dirname, 'views'));
+
+//controller
 app.get('/', function (req, res) {
-    console.log(req);
-    res.send('<h1>Cool, it is running! or is it?</h1>');
+    //console.log(req);
+    /*console.log(__dirname); //it displays the directory from where server is started.
+    res.send('<h1>Cool, it is running! or is it?</h1>');*/
+
+    return res.render('home', {
+        title: 'Contacts List'
+    });
+});
+
+app.get('/practice', function (req, res) {
+    return res.render('practice', {
+        title: 'Let us play with ejs'
+    });
 });
 
 app.listen(port, function(err){
