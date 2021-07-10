2b45780fa058a2227d8f7f39f2ebe30d7fd05f81
Piyush Pandey <piyushpvns@gmail.com>
2021-07-11 00:54:33 +0530

    Update mongoose.js

--- Contact_List/config/mongoose.js
+++ Contact_List/config/mongoose.js
@@ -1,18 +1,22 @@
 //require the library
 const mongoose = require('mongoose');
 
-//connect to the database
-mongoose.connect('mongodb://localhost/contacts_list_db');
 
-//acquire the connection to check if it is successful
+//connect to the database
+mongoose.connect('mongodb://localhost/contacts_list_db', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+//*! acquire the connection to check if it is successful.
 const db = mongoose.connection;
 
 //Note - js is event driven language
 
+
 //on error
-db.model('error', cosnole.error.bind(console, 'error connecting to db'));
+db.on('error', console.error.bind(console, 'error connecting to db'));
+
 
 //if the connection up and running then print the message
 db.once('open', function(){
-    console.log('successfully connected to db');
+    console.log('Successfully connected to the DataBase');
 });
