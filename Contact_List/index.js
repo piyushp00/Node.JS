50fa364a9cbab16071a1ea670965799a5ca438c4
Piyush Pandey <piyushpvns@gmail.com>
2021-07-10 22:14:57 +0530

    Update index.js

--- Contact_List/index.js
+++ Contact_List/index.js
@@ -1,21 +1,19 @@
 //code order -> 1.ibraries - 2.different files and properties- 3.middleware
-const express = require('express');
+const express = require("express");
 //library for storing views(comes in built)
-const path = require('path');
+const path = require("path");
 const port = 8000;
 
 const app = express();
 
 //setting up template engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 //store views/templates
-app.set('views', path.join(__dirname, 'views'));
-
-
+app.set("views", path.join(__dirname, "views"));
 
 //parser(middleware)
 app.use(express.urlencoded());
-app.use(express.static('assets'))
+app.use(express.static("assets"));
 
 /*
 //our own middleware
@@ -34,98 +32,88 @@ app.use(function(req, res, next) {
 });
 */
 
-
-
-
-
-
-
 //Create Contacts List
 var contactsList = [
-    {
-        name: "Piyush",
-        phone: "7275740356"
-    },
-    {
-        name: "Arpan ",
-        phone: "9857430451"
-    },
-    {
-        name: "Tony Stark",
-        phone: "0123456789"
-    },
-    {
-        name: "Darth Vader",
-        phone: "9876543210"
-    },
-    {
-        name: "Luke Skywalker",
-        phone: "9988888899"
-    }
-]; 
-
+  {
+    name: "Piyush",
+    phone: "7275740356",
+  },
+  {
+    name: "Arpan ",
+    phone: "9857430451",
+  },
+  {
+    name: "Tony Stark",
+    phone: "0123456789",
+  },
+  {
+    name: "Darth Vader",
+    phone: "9876543210",
+  },
+  {
+    name: "Luke Skywalker",
+    phone: "9988888899",
+  },
+];
 
 //Controller-1 (rendering home page)
-app.get('/', function (req, res) {
-    //console.log(req);
-    /*console.log(__dirname); //it displays the directory from where server is started.
+app.get("/", function (req, res) {
+  //console.log(req);
+  /*console.log(__dirname); //it displays the directory from where server is started.
     res.send('<h1>Cool, it is running! or is it?</h1>');*/
 
-    return res.render('home', {
-        title: 'Contacts List',
-        contact_list: contactsList
-    }); 
+  return res.render("home", {
+    title: "Contacts List",
+    contact_list: contactsList,
+  });
 });
-
 
 //Controller-2 (rendering practice page)
-app.get('/practice', function (req, res) {
-    return res.render('practice', {
-        title: 'Let us play with ejs' //context is being passes to views 
-    });
+app.get("/practice", function (req, res) {
+  return res.render("practice", {
+    title: "Let us play with ejs", //context is being passes to views
+  });
 });
 
-
 //Controller-3 (create contact)
-app.post('/create-contact', function(req, res){
-    console.log(req.body);
-    /*contactsList.push({
+app.post("/create-contact", function (req, res) {
+  console.log(req.body);
+  /*contactsList.push({
         name: req.body.name,
         phone: req.body.phone
     });*/
 
-    //or we can write
-    contactsList.push(req.body);
-    //return res.redirect('/');
-    //for coming to same page we can also write as below
-    return res.redirect('back')
+  //or we can write
+  contactsList.push(req.body);
+  //return res.redirect('/');
+  //for coming to same page we can also write as below
+  return res.redirect("back");
 });
-
 
 //Controller-4 (delete contact)
-app.get('/delete-contact/', function(req, res){
-    //with params
-    //let phone = req.params.phone;
-    //with query params
-    console.log(req.query);
-    //get the query from the url
-    let phone  = req.query.phone;
-    
-    let contactIndex = contactsList.findIndex(contact => contact.phone == phone)
-    
-    if(contactIndex != -1){
-        contactsList.splice(contactIndex, 1);
-    }
+app.get("/delete-contact/", function (req, res) {
+  //with params
+  //let phone = req.params.phone;
+  //with query params
+  console.log(req.query);
+  //get the query from the url
+  let phone = req.query.phone;
 
-    return res.redirect('back');
+  let contactIndex = contactsList.findIndex(
+    (contact) => contact.phone == phone
+  );
 
+  if (contactIndex != -1) {
+    contactsList.splice(contactIndex, 1);
+  }
+
+  return res.redirect("back");
 });
 
+app.listen(port, function (err) {
+  if (err) {
+    console.log("Error in running the server", err);
+  }
 
-app.listen(port, function(err){
-    if(err){
-        console.log('Error in running the server', err);
-    }
-
-    console.log('Yup! My Express Server is running on Port', port);
+  console.log("Yup! My Express Server is running on Port", port);
 });
